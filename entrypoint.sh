92bcfe3621d65885be9c7f06b76de545422584d9
Matt Jones <gitcode@magisa.org>
2021-11-08 18:20:29 -0900

    Update group and permissions for files.

--- entrypoint.sh
+++ entrypoint.sh
@@ -25,11 +25,11 @@ if [[ "$INPUT_KEY" ]]; then
         chmod 0700 /root/.ssh && \
         ssh-keyscan ${INPUT_HOST} > /root/.ssh/known_hosts
     #scp -qr -P $INPUT_PORT -o StrictHostKeyChecking=no -i tmp_id $INPUT_SOURCE "$INPUT_USERNAME"@"$INPUT_HOST":"$INPUT_TARGET"
-    echo "ssh -v -p ${INPUT_PORT} -o StrictHostKeyChecking=no -i tmp_id ${INPUT_USERNAME}@${INPUT_HOST} 'mkdir -p $INPUT_TARGET'"
-    ssh -v -p ${INPUT_PORT} -o StrictHostKeyChecking=no -i tmp_id ${INPUT_USERNAME}@${INPUT_HOST} "mkdir -p $INPUT_TARGET"
+    ssh -p ${INPUT_PORT} -i tmp_id ${INPUT_USERNAME}@${INPUT_HOST} "mkdir -p $INPUT_TARGET"
     rsync -rav -e "ssh -i tmp_id -p ${INPUT_PORT}" ${INPUT_SOURCE} $INPUT_USERNAME@$INPUT_HOST:$INPUT_TARGET
+    ssh -p ${INPUT_PORT} ${INPUT_USERNAME}@${INPUT_HOST} "chgrp -R web-dev ${INPUT_TARGET}; chmod -R g+rwx ${INPUT_TARGET}"
     #ssh -p ${INPUT_PORT} $INPUT_USERNAME@$INPUT_HOST "if [ -d ${INPUT_PUBLISH}_old ]; then rm -r ${INPUT_PUBLISH}_old; fi; if [ -d ${INPUT_PUBLISH} ]; then mv ${INPUT_PUBLISH} ${INPUT_PUBLISH}_old; fi; mv ${INPUT_TARGET} ${INPUT_PUBLISH}; chgrp -R web-dev ${INPUT_PUBLISH}; chmod -R g+rwx ${INPUT_PUBLISH}"
-    #echo "Transfer process complete using SSH keys"
+    echo "Transfer process complete using rsyn over SSH keys"
 else
     echo "Trying password authentication as key is not available"
     #sshpass -p $INPUT_PASSWORD scp -qr -P $INPUT_PORT -o StrictHostKeyChecking=no $INPUT_SOURCE "$INPUT_USERNAME"@"$INPUT_HOST":"$INPUT_TARGET"
