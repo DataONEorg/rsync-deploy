275f50b9fe54dd6d706696f522b19cd06ca7cade
Matt Jones <gitcode@magisa.org>
2021-11-08 17:32:01 -0900

    Debug the entrypoint.

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,7 @@
 # Deploy script run during a lifecycle event
 # The script has access to the following environment variables that are configured
 # as secrets:
+# INPUT_KEY: ssh private key of the user to be logged in on the deployment host
 # INPUT_USER: username of the user to be logged in on the deployment host
 # INPUT_HOST: the target host for deployment
 # INPUT_SOURCE: the directory to copy files from
@@ -24,9 +25,9 @@ if [[ "$INPUT_KEY" ]]; then
         chmod 0700 ~/.ssh && \
         ssh-keyscan ${INPUT_HOST} > ~/.ssh/known_hosts
     #scp -qr -P $INPUT_PORT -o StrictHostKeyChecking=no -i tmp_id $INPUT_SOURCE "$INPUT_USERNAME"@"$INPUT_HOST":"$INPUT_TARGET"
-    #echo "ssh -v -p ${INPUT_PORT} -i tmp_id ${INPUT_USER}@${INPUT_HOST} 'mkdir -p $INPUT_TARGET'"
+    echo "ssh -v -p ${INPUT_PORT} -o StrictHostKeyChecking=no -i tmp_id ${INPUT_USER}@${INPUT_HOST} 'mkdir -p $INPUT_TARGET'"
     ssh -v -p ${INPUT_PORT} -o StrictHostKeyChecking=no -i tmp_id ${INPUT_USER}@${INPUT_HOST} "mkdir -p $INPUT_TARGET"
-    rsync -rav -e "ssh -i tmp_id -p ${INPUT_PORT}" ${INPUT_SOURCE} $INPUT_USER@$INPUT_HOST:$INPUT_TARGET
+    #rsync -rav -e "ssh -i tmp_id -p ${INPUT_PORT}" ${INPUT_SOURCE} $INPUT_USER@$INPUT_HOST:$INPUT_TARGET
     #ssh -p ${INPUT_PORT} $INPUT_USER@$INPUT_HOST "if [ -d ${INPUT_PUBLISH}_old ]; then rm -r ${INPUT_PUBLISH}_old; fi; if [ -d ${INPUT_PUBLISH} ]; then mv ${INPUT_PUBLISH} ${INPUT_PUBLISH}_old; fi; mv ${INPUT_TARGET} ${INPUT_PUBLISH}; chgrp -R web-dev ${INPUT_PUBLISH}; chmod -R g+rwx ${INPUT_PUBLISH}"
     #echo "Transfer process complete using SSH keys"
 else
