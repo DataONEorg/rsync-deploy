e9d5d06bb28939e83a18c954b0aa7c8aa90ff974
Matt Jones <gitcode@magisa.org>
2021-11-08 18:09:46 -0900

    Fix USERNAME variable.

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@
 # The script has access to the following environment variables that are configured
 # as secrets:
 # INPUT_KEY: ssh private key of the user to be logged in on the deployment host
-# INPUT_USER: username of the user to be logged in on the deployment host
+# INPUT_USERNAME: username of the user to be logged in on the deployment host
 # INPUT_HOST: the target host for deployment
 # INPUT_SOURCE: the directory to copy files from
 # INPUT_TARGET: the directory for files to be copied to on the deployment host
@@ -25,10 +25,10 @@ if [[ "$INPUT_KEY" ]]; then
         chmod 0700 /root/.ssh && \
         ssh-keyscan ${INPUT_HOST} > /root/.ssh/known_hosts
     #scp -qr -P $INPUT_PORT -o StrictHostKeyChecking=no -i tmp_id $INPUT_SOURCE "$INPUT_USERNAME"@"$INPUT_HOST":"$INPUT_TARGET"
-    echo "ssh -v -p ${INPUT_PORT} -o StrictHostKeyChecking=no -i tmp_id ${INPUT_USER}@${INPUT_HOST} 'mkdir -p $INPUT_TARGET'"
-    ssh -v -p ${INPUT_PORT} -o StrictHostKeyChecking=no -i tmp_id ${INPUT_USER}@${INPUT_HOST} "mkdir -p $INPUT_TARGET"
-    #rsync -rav -e "ssh -i tmp_id -p ${INPUT_PORT}" ${INPUT_SOURCE} $INPUT_USER@$INPUT_HOST:$INPUT_TARGET
-    #ssh -p ${INPUT_PORT} $INPUT_USER@$INPUT_HOST "if [ -d ${INPUT_PUBLISH}_old ]; then rm -r ${INPUT_PUBLISH}_old; fi; if [ -d ${INPUT_PUBLISH} ]; then mv ${INPUT_PUBLISH} ${INPUT_PUBLISH}_old; fi; mv ${INPUT_TARGET} ${INPUT_PUBLISH}; chgrp -R web-dev ${INPUT_PUBLISH}; chmod -R g+rwx ${INPUT_PUBLISH}"
+    echo "ssh -v -p ${INPUT_PORT} -o StrictHostKeyChecking=no -i tmp_id ${INPUT_USERNAME}@${INPUT_HOST} 'mkdir -p $INPUT_TARGET'"
+    ssh -v -p ${INPUT_PORT} -o StrictHostKeyChecking=no -i tmp_id ${INPUT_USERNAME}@${INPUT_HOST} "mkdir -p $INPUT_TARGET"
+    rsync -rav -e "ssh -i tmp_id -p ${INPUT_PORT}" ${INPUT_SOURCE} $INPUT_USERNAME@$INPUT_HOST:$INPUT_TARGET
+    #ssh -p ${INPUT_PORT} $INPUT_USERNAME@$INPUT_HOST "if [ -d ${INPUT_PUBLISH}_old ]; then rm -r ${INPUT_PUBLISH}_old; fi; if [ -d ${INPUT_PUBLISH} ]; then mv ${INPUT_PUBLISH} ${INPUT_PUBLISH}_old; fi; mv ${INPUT_TARGET} ${INPUT_PUBLISH}; chgrp -R web-dev ${INPUT_PUBLISH}; chmod -R g+rwx ${INPUT_PUBLISH}"
     #echo "Transfer process complete using SSH keys"
 else
     echo "Trying password authentication as key is not available"
