5e5b44b591018a94badd4a7701afdaefd28b8440
Matt Jones <gitcode@magisa.org>
2021-11-08 16:56:11 -0900

    Debug rsync command.

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,12 +15,19 @@
 echo "+++++++++++++++++++STARTING TRANSFER+++++++++++++++++++"
 
 if [[ "$INPUT_KEY" ]]; then
+    #echo -n "Working dir: "
+    #echo `pwd`
     echo -e "${INPUT_KEY}" > tmp_id
     chmod 600 tmp_id
+    #echo `ifconfig`
+    #mkdir -p /root/.ssh && \
+    #    chmod 0700 /root/.ssh && \
+    #    ssh-keyscan ${INPUT_HOST} > /root/.ssh/known_hosts
     #scp -qr -P $INPUT_PORT -o StrictHostKeyChecking=no -i tmp_id $INPUT_SOURCE "$INPUT_USERNAME"@"$INPUT_HOST":"$INPUT_TARGET"
-    ssh -P ${INPUT_PORT} $INPUT_USER@$INPUT_HOST "mkdir -p $INPUT_TARGET"
-    rsync -rav -e "ssh -i tmp_id" ${INPUT_SOURCE} $INPUT_USER@$INPUT_HOST:$INPUT_TARGET
-    #ssh -P ${INPUT_PORT} $INPUT_USER@$INPUT_HOST "if [ -d ${INPUT_PUBLISH}_old ]; then rm -r ${INPUT_PUBLISH}_old; fi; if [ -d ${INPUT_PUBLISH} ]; then mv ${INPUT_PUBLISH} ${INPUT_PUBLISH}_old; fi; mv ${INPUT_TARGET} ${INPUT_PUBLISH}; chgrp -R web-dev ${INPUT_PUBLISH}; chmod -R g+rwx ${INPUT_PUBLISH}"
+    #echo "ssh -v -p ${INPUT_PORT} -i /tmp_id ${INPUT_USER}@${INPUT_HOST} 'mkdir -p $INPUT_TARGET'"
+    ssh -p ${INPUT_PORT} -i /tmp_id ${INPUT_USER}@${INPUT_HOST} "mkdir -p $INPUT_TARGET"
+    rsync -rav -e "ssh -i tmp_id -p ${INPUT_PORT}" ${INPUT_SOURCE} $INPUT_USER@$INPUT_HOST:$INPUT_TARGET
+    #ssh -p ${INPUT_PORT} $INPUT_USER@$INPUT_HOST "if [ -d ${INPUT_PUBLISH}_old ]; then rm -r ${INPUT_PUBLISH}_old; fi; if [ -d ${INPUT_PUBLISH} ]; then mv ${INPUT_PUBLISH} ${INPUT_PUBLISH}_old; fi; mv ${INPUT_TARGET} ${INPUT_PUBLISH}; chgrp -R web-dev ${INPUT_PUBLISH}; chmod -R g+rwx ${INPUT_PUBLISH}"
     #echo "Transfer process complete using SSH keys"
 else
     echo "Trying password authentication as key is not available"
